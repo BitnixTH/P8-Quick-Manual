0c29cd690c8633ac53831dba8f17d1a2adc1d4c9
BitnixTH <parknahpus.bitnix@gmail.com>
2026-09-04 16:29:10 +0700

    Fix mobile video scroll message size

--- script.js
+++ script.js
@@ -1951,6 +1951,7 @@ const videoScrollHint =
   hasTopicVideos
     ? `
       <div
+        class="video-scroll-message"
         style="
           text-align: center;
           font-weight: 800;
